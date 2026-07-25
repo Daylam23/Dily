df9797a00c0a33cf930584341f5acb67e3ec4f1e
soy Daylam <ballesterosdylan77@gmail.com>
2026-07-24 21:18:56 -0300

    modificando nombre

--- codigo.py
+++ codigo.py
@@ -1,5 +1,9 @@
 nombre = "PEPE"
 apellido = "Ballesteros"
 
+<<<<<<< HEAD
 saludo = "hola "+ nombre + " " + apellido + "¿Como estas?"
+=======
+saludo = "hola "+ nombre + " " + apellido + " ¿Como estas?"
+>>>>>>> 4152f (modificando nombre)
 print(saludo)
